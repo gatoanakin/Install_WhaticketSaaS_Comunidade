1f9a508a7c9627289f5f1368769acfeb32e7e22d
gatoanakin <147204355+gatoanakin@users.noreply.github.com>
2023-10-14 05:18:15 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -25,15 +25,15 @@ printf "${GREEN}";
  printf "                                              .:^^^^^^^^^^:.        \n";
  printf "                                            :~^^!7??JJ??7!~^^^.     \n";
  printf "                                          .~^~?YY!?YYYYY5YYJ!^~^    \n";
- printf "                                         :!:7YYY^ :YYYYJ~!YYYJ^^!   \n";
- printf "                                        .7.7YYY~   JYY?: .YYYYY:~^  \n";
+ printf "                                         :!:7YYY^        !YYYJ^^!   \n";
+ printf "                                        .7.7YYY~   JYY?  .YYYYY:~^  \n";
  printf "                                        ^!:YYY~    ?Y!   .JYYYY7.7  \n";
- printf "                                        ^!:YY!  :  ::    .YYJ?Y7.7  \n";
- printf "                                        .7.?!  77    :7: .7!~YY:~^  \n";
- printf "                                         ~J! :JYJ^.^7Y5~  :!YJ^^!   \n";
- printf "                                         ?7:!!?YYYYYYYYY??YJ!^~^    \n";
- printf "                                        ^J!?7~^~!7??JJJ??!~^^^.     \n";
- printf "                                        ^~:   .:^^^^^^^^^^:.        \n";
+ printf "                                        ^!:YY!           .YYJ?Y7.7  \n";
+ printf "                                        .7.?!  779:7YYYYYY7!~YY:~^  \n";
+ printf "                                         ~J!  :JYJ^.^7Y5~YY:!YJ^!   \n";
+ printf "                                         ?7:!!?YYYYYYYYY??YJ!^^     \n";
+ printf "                                        ^J!?7~^~!7??JJJ??!^^        \n";
+ printf "                                        ^~:                         \n";
 
   printf "\n"
 
